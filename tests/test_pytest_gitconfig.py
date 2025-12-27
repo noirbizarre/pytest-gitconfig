8b1e5a23e5e15be811d05ec10b6fafbb7fcb438e
Axel H. <noirbizarre@gmail.com>
2025-12-27 23:13:16 +0100

    test(set): add a test case for mixed dict

--- tests/test_pytest_gitconfig.py
+++ tests/test_pytest_gitconfig.py
@@ -93,6 +93,14 @@ def test_gitconfig_set_nested_dicts(gitconfig: GitConfig):
     assert get_config("test.nestedDicts") == expected
 
 
+def test_gitconfig_set_mixed_dict(gitconfig: GitConfig):
+    expected = "expected"
+    gitconfig.set({"test.dottedDict": expected, "test": {"nestedDicts": expected}})
+
+    assert get_config("test.dottedDict") == expected
+    assert get_config("test.nestedDicts") == expected
+
+
 def test_gitconfig_set_key_value(gitconfig: GitConfig):
     expected = "expected"
     gitconfig.set("test.dottedDict", expected)
